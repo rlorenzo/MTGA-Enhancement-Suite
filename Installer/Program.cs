dabe4d730270d644d1cd0189e6c6073419f78f16
Dan <dan@mayer.cool>
2026-04-05 00:06:35 -0700

    Show install result in exe installer with output capture

--- Installer/Program.cs
+++ Installer/Program.cs
@@ -23,12 +23,60 @@ namespace MTGAESInstaller
                 FileName = "powershell.exe",
                 Arguments = "-ExecutionPolicy Bypass -Command \"irm https://raw.githubusercontent.com/MayerDaniel/MTGA-Enhancement-Suite/main/install.ps1 | iex\"",
                 UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
             };
 
             try
             {
                 var proc = Process.Start(psi);
+
+                // Read output in real-time
+                proc.OutputDataReceived += (s, e) =>
+                {
+                    if (e.Data != null) Console.WriteLine(e.Data);
+                };
+                proc.ErrorDataReceived += (s, e) =>
+                {
+                    if (e.Data != null)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(e.Data);
+                        Console.ResetColor();
+                    }
+                };
+
+                proc.BeginOutputReadLine();
+                proc.BeginErrorReadLine();
                 proc.WaitForExit();
+
+                Console.WriteLine();
+                if (proc.ExitCode == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("  ==============================");
+                    Console.WriteLine("    Installation successful!");
+                    Console.WriteLine("  ==============================");
+                    Console.ResetColor();
+                    Console.WriteLine();
+                    Console.WriteLine("  Launch MTGA and look for the MTGA+ button");
+                    Console.WriteLine("  in the top navigation bar.");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("  ==============================");
+                    Console.WriteLine("    Installation failed!");
+                    Console.WriteLine("  ==============================");
+                    Console.ResetColor();
+                    Console.WriteLine();
+                    Console.WriteLine("  Check the output above for errors.");
+                    Console.WriteLine("  You may need to run this installer as Administrator.");
+                }
+
+                Console.WriteLine();
+                Console.Write("  Press any key to exit...");
+                Console.ReadKey(true);
                 return proc.ExitCode;
             }
             catch (Exception ex)
@@ -36,6 +84,8 @@ namespace MTGAESInstaller
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine($"  ERROR: {ex.Message}");
                 Console.ResetColor();
+                Console.WriteLine();
+                Console.WriteLine("  Make sure PowerShell is installed on your system.");
                 Console.WriteLine();
                 Console.Write("  Press any key to exit...");
                 Console.ReadKey(true);
